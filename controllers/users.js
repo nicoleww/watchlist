e61cf0be7150b99f614313b063a84eaf36a3c2c7
Nicole WItherall <nicwitherall@gmail.com>
2021-03-31 11:26:37 -0400

    Initialize with OAuth and DB set up only

---
+++ controllers/users.js
@@ -0,0 +1,23 @@
+const User = require('../models/user');
+
+module.exports = {
+    index,
+}
+
+function index(req, res, next) {
+    console.log(req.user);
+
+    let modelQuery = req.query.name ? {name: new RegExp(req.query.name, 'i')} : {};
+
+    let sortKey = req.query.sort || 'name';
+    User.find(modelQuery)
+    .sort(sortKey).exec(function(err, users) {
+      if (err) return next(err);
+
+      res.render('user/index', { 
+      
+        user: req.user, 
+        name: req.query.name, 
+        sortKey });
+    });
+  }
---
+++ routes/users.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var router = express.Router();
+const usersCtrl = require('../controllers/users');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  console.log(req.user);
+  res.render('index.ejs', {user : req.user});
+});
+
+// router.get('/account', usersCtrl.viewAccount);
+// router.get('/new', usersCtrl.addPet);
+
+module.exports = router;
